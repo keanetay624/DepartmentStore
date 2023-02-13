9b9e305a41110f21852b96dd5fa7e72c6db7550e
keane <keanetay@gmail.com>
2023-02-13 16:44:48 +0800

    Update Model - SalesItem.kt

--- src/main/kotlin/com/keanetay/DepartmentStore/model/SalesItem.kt
+++ src/main/kotlin/com/keanetay/DepartmentStore/model/SalesItem.kt
@@ -1,13 +1,21 @@
 package com.keanetay.DepartmentStore.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.math.BigDecimal
 import java.util.*
+import javax.persistence.*
 
+@Entity
+@Table
 class SalesItem(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Int,
         val invoiceNo: String,
         val stockCode: String,
         val description: String,
         val quantity: Long,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="MM/dd/yyyy HH:mm:ss a")
         val invoiceDate: Date,
         val unitPrice: BigDecimal,
         val customerId: Long,
