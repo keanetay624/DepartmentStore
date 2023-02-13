e3d54db0e53201e114a868b7f40efa34d6f407a1
keane <keanetay@gmail.com>
2023-02-13 09:34:35 +0800

    initialize spring boot

---
+++ src/main/kotlin/com/keanetay/DepartmentStore/DepartmentStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.keanetay.DepartmentStore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DepartmentStoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<DepartmentStoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/keanetay/DepartmentStore/DepartmentStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.keanetay.DepartmentStore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DepartmentStoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
