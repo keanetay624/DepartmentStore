580fe5b910253acb451a97d675cc5062832c4238
keane <keanetay@gmail.com>
2023-02-14 14:43:54 +0800

    Add CSVService, update SalesItemController

--- src/main/kotlin/com/keanetay/DepartmentStore/controller/SalesItemController.kt
+++ src/main/kotlin/com/keanetay/DepartmentStore/controller/SalesItemController.kt
@@ -1,21 +1,40 @@
 package com.keanetay.DepartmentStore.controller
 
 import com.keanetay.DepartmentStore.model.SalesItem
-import com.keanetay.DepartmentStore.repository.SalesItemRepository
+import com.keanetay.DepartmentStore.service.CSVService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("api/v1")
 class SalesItemController {
     @Autowired
-    lateinit var salesItemRepository : SalesItemRepository
-    @PostMapping("/saveSalesItem")
-    fun save(@RequestBody salesItem: SalesItem):String {
-        salesItemRepository.save(salesItem)
-        return "Sales Item saved!"
+    lateinit var csvService : CSVService
+
+    @PostMapping("upload")
+    fun uploadFile(@RequestParam("file") file: MultipartFile): ResponseEntity<String?>? {
+        var message: String
+        val error: String
+        if (file.isEmpty) {
+            message = "File: " + file.originalFilename + " is empty!"
+            error = "Empty files are not permitted"
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body<String?>(message)
+        }
+//        if (!CSVUtil.hasCSVFormat(file)) {
+//            message = "Please upload a csv file!"
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body<String?>(message)
+//        }
+        return try {
+            val list : List<SalesItem> = csvService.saveSalesItems(file)
+            message = "Uploaded the file successfully: " + file.originalFilename
+            ResponseEntity.status(HttpStatus.OK).body<String?>(message)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            message = "Could not upload the file: " + file.originalFilename + "!"
+            ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body<String?>(message)
+        }
     }
 }
---
+++ src/main/kotlin/com/keanetay/DepartmentStore/service/CSVService.kt
@@ -0,0 +1,28 @@
+package com.keanetay.DepartmentStore.service
+
+import com.keanetay.DepartmentStore.model.SalesItem
+import com.keanetay.DepartmentStore.repository.SalesItemRepository
+import com.keanetay.DepartmentStore.util.CSVUtil
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.io.IOException
+
+@Service
+class CSVService {
+    @Autowired
+    lateinit var salesItemRepository : SalesItemRepository
+
+    fun saveSalesItems(file: MultipartFile): List<SalesItem> {
+        val list : List<SalesItem>
+        try {
+            list = file.inputStream.use { inputStream ->
+                CSVUtil.readCsv(inputStream)
+            }
+            salesItemRepository.saveAll(list)
+        } catch (e: IOException) {
+            throw RuntimeException("fail to store csv data: " + e.message)
+        }
+        return list
+    }
+}
