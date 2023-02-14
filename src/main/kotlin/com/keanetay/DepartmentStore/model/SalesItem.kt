20dafd5aaef73bcb65fa5d2d3595dfe94129b5ad
keane <keanetay@gmail.com>
2023-02-14 14:43:21 +0800

    Update SalesItem model to use LocalDate instead of Date

--- src/main/kotlin/com/keanetay/DepartmentStore/model/SalesItem.kt
+++ src/main/kotlin/com/keanetay/DepartmentStore/model/SalesItem.kt
@@ -2,7 +2,7 @@ package com.keanetay.DepartmentStore.model
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import java.math.BigDecimal
-import java.util.*
+import java.time.LocalDate
 import javax.persistence.*
 
 @Entity
@@ -15,8 +15,8 @@ class SalesItem(
         val stockCode: String,
         val description: String,
         val quantity: Long,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="MM/dd/yyyy HH:mm:ss a")
-        val invoiceDate: Date,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="MM/d/yyyy H:mm")
+        val invoiceDate: LocalDate,
         val unitPrice: BigDecimal,
         val customerId: Long,
         val country: String
