ff8ba5c47a424fb3d0afbd270d2223bbb9d7e4af
keane <keanetay@gmail.com>
2023-02-16 09:30:47 +0800

    Add pagination to get request

--- src/main/kotlin/com/keanetay/DepartmentStore/service/CSVService.kt
+++ src/main/kotlin/com/keanetay/DepartmentStore/service/CSVService.kt
@@ -4,6 +4,9 @@ import com.keanetay.DepartmentStore.model.SalesItem
 import com.keanetay.DepartmentStore.repository.SalesItemRepository
 import com.keanetay.DepartmentStore.util.CSVUtil
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.IOException
@@ -29,7 +32,12 @@ class CSVService {
     fun getSalesItems(searchStr: String,
                       limit: String,
                       offset: String): List<SalesItem> {
-        val list : List<SalesItem> = salesItemRepository.getSalesItemsBySearchStr(searchStr)
-        return list
+        val paging: Pageable = PageRequest.of(offset.toInt(), limit.toInt())
+        val pageSalesItems: Page<SalesItem> = salesItemRepository.getSalesItemsBySearchStr(searchStr, paging)
+
+        if (!pageSalesItems.isEmpty()) {
+            return  pageSalesItems.content
+        }
+        return listOf<SalesItem>()
     }
 }
