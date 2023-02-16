6a2b2200fdf9d72e821fe691cebb4aabd404280f
keane <keanetay@gmail.com>
2023-02-16 16:22:34 +0800

    Refactor CSVUtil

--- src/main/kotlin/com/keanetay/DepartmentStore/util/CSVUtil.kt
+++ src/main/kotlin/com/keanetay/DepartmentStore/util/CSVUtil.kt
@@ -3,9 +3,11 @@ package com.keanetay.DepartmentStore.util
 import com.keanetay.DepartmentStore.model.SalesItem
 import mu.KotlinLogging
 import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
 import java.io.InputStream
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 object CSVUtil {
     private const val PATTERN = "M/d/yyyy H:mm"
@@ -14,24 +16,37 @@ object CSVUtil {
 
     fun readCsv(inputStream: InputStream): List<SalesItem> {
         logger.info("---- CSVUtil - readCsv Start ----")
-        val salesItemList: List<SalesItem> = CSVFormat.Builder
+        val salesItemList:MutableList<SalesItem> = mutableListOf<SalesItem>()
+        val parser: CSVParser = CSVFormat.Builder
             .create(CSVFormat.EXCEL)
-            .build().parse(inputStream.reader())
-            .drop(1) // Dropping the header
-            .map {
-                SalesItem(
+            .setHeader()
+            .setSkipHeaderRecord(true)
+            .build()
+            .parse(inputStream.reader())
+
+        for (csvRecord in parser) {
+            try {
+                logger.info("attempting to cast into sales item")
+                val item = SalesItem(
                     id = 0,
-                    invoiceNo = it[0],
-                    stockCode = it[1],
-                    description = it[2],
-                    quantity = it[3].toLongOrNull(),
-                    invoiceDate = LocalDateTime.parse(it[4], formatter),
-                    unitPrice = it[5].toBigDecimalOrNull(),
-                    customerId = it[6].toLongOrNull(),
-                    country = it[7]
+                    invoiceNo = csvRecord[0],
+                    stockCode = csvRecord[1],
+                    description = csvRecord[2],
+                    quantity = csvRecord[3].toLongOrNull(),
+                    invoiceDate = LocalDateTime.parse(csvRecord[4], formatter),
+                    unitPrice = csvRecord[5].toBigDecimalOrNull(),
+                    customerId = csvRecord[6].toLongOrNull(),
+                    country = csvRecord[7]
                 )
+                salesItemList.add(item)
+            } catch (ex: DateTimeParseException) {
+                val invoiceNo:String = csvRecord[0]
+                logger.warn("failed to parse datetime for invoiceNo: $invoiceNo")
             }
+        }
         logger.info("---- CSVUtil - readCsv End ----")
         return salesItemList
     }
+
+
 }
