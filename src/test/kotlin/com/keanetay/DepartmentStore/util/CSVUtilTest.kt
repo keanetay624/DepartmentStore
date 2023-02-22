4e03229c38619c4f74266e8e00b509c14cbe9c7c
keane <keanetay@gmail.com>
2023-02-22 18:03:42 +0800

    Add unit tests for CSVUtil

--- src/test/kotlin/com/keanetay/DepartmentStore/util/CSVUtilTest.kt
+++ src/test/kotlin/com/keanetay/DepartmentStore/util/CSVUtilTest.kt
@@ -1,15 +1,22 @@
 package com.keanetay.DepartmentStore.util
 
-import org.junit.jupiter.api.Test
-
+import com.keanetay.DepartmentStore.model.SalesItem
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.io.File
+
 
 internal class CSVUtilTest {
-
+    private val filepath:String = System.getProperty("user.dir") + "/src/main/resources/testFiles/Test_Exception.csv"
     @Test
-    @DisplayName("Test main function in readCsv")
+    @DisplayName("Should return 1 item")
     fun readCsv() {
-        assertEquals(true, true)
+        val file = File(filepath)
+        val salesItems:List<SalesItem>
+        file.inputStream().use {
+            salesItems = CSVUtil.readCsv(it)
+        }
+        assertEquals(salesItems.size,1)
     }
 }
